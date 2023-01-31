8de2f94af866a29fcba09498ba0f985b9f3d4ac2
Medmly20208 <moulaymohamed856@gmail.com>
2023-01-31 18:31:38 +0100

    test api in client side

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const carRouter = require("./routes/carRoute");
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/v1/cars", carRouter);
 
